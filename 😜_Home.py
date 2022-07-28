02993f9359da83d47c0275e1fe994baa1c47edf9
Janrswong <janrswong@addu.edu.ph>
2022-07-29 00:29:04 +0800

    added sd, improved charts
    Co-authored-by: Stephanie Lorraine Ignas <ignasstephanie@users.noreply.github.com>

--- 😜_Home.py
+++ 😜_Home.py
@@ -1,3 +1,5 @@
+# TODO: import daily, add combined data w selection, finalize home
+from distutils.command import sdist
 from statsmodels.tsa.arima_model import ARIMAResults
 import streamlit as st
 import pandas as pd
@@ -16,7 +18,7 @@ from keras.layers import Dense, LSTM
 # from tensorflow.keras import layers
 # from tensorflow.keras.optimizers import Adam
 # from tensorflow.keras.callbacks import ModelCheckpoint
-from st_aggrid import AgGrid
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
 
 # page expands to full width
 st.set_page_config(page_title="LSTM vs ARIMA", layout='wide')
@@ -51,11 +53,46 @@ st.header("Raw Data")
 # using button
 # if st.button('Press to see Brent Crude Oil Raw Data'):
 df = yf.download('BZ=F', interval=getInterval(interv[0]))
-st.dataframe(df.head())
+# st.dataframe(df.head())
+
+df = df.reset_index()
+
+# TODO: find AgGrid customization
+
+
+def pagination(df):
+    gb = GridOptionsBuilder.from_dataframe(df)
+    gb.configure_pagination(paginationAutoPageSize=True)
+    return gb.build()
+
+
+# enable enterprise modules for trial only
+# raw data
+page = pagination(df)
+AgGrid(df, enable_enterprise_modules=True,
+       theme='streamlit', gridOptions=page, fit_columns_on_grid_load=True, key='data')
 
 # TODO: standard deviation
-# TODO: find AgGrid customization 
-# AgGrid(df)
+st.header("Standard Deviation")
+sd = pd.read_csv('StandardDeviation.csv')
+sd.drop("Unnamed: 0", axis=1, inplace=True)
+sd = sd.reset_index()
+
+AgGrid(sd, key='SD1', enable_enterprise_modules=True,
+       fit_columns_on_grid_load=True, theme='streamlit')
+
+sd = sd.pivot(index='Start Date', columns='Interval',
+              values='Standard Deviation')
+sd = sd.reset_index()
+# table
+# AgGrid(sd, key='SD', enable_enterprise_modules=True,
+#        fit_columns_on_grid_load=True, domLayout='autoHeight', theme='streamlit')
+
+# visualization
+fig = px.line(sd, x=sd.index, y=['1d', '1wk', '1mo', '3mo'],
+              title="STANDARD DEVIATION OF BRENT CRUDE OIL PRICES", width=1000)
+st.plotly_chart(fig, use_container_width=True)
+
 
 # accuracy metrics
 st.header("Accuracy Metric Comparison")
@@ -63,8 +100,11 @@ st.header("Accuracy Metric Comparison")
 # LSTM METRICS
 st.write("LSTM Metrics")
 readfile = pd.read_csv('LSTM.csv')
-readfile.drop("Unnamed: 0", axis=1)
-AgGrid(readfile, key='LSTMMetric')
+readfile.drop("Unnamed: 0", axis=1, inplace=True)
+AgGrid(readfile, key='LSTMMetric', fit_columns_on_grid_load=True,
+       enable_enterprise_modules=True, theme='streamlit')
+
+# TODO: Import ARIMA-DAILY.csv adn uncomment code
 
 # ARIMA METRICS
 st.write("ARIMA Metrics")
@@ -73,23 +113,31 @@ intervals = st.selectbox(
 
 if intervals == 'Weekly':
     file = pd.read_csv('ARIMAMetrics/ARIMA-WEEKLY.csv')
-    file.drop("Unnamed: 0", axis=1)
-    AgGrid(file, key='weeklyMetric')
+    file.drop("Unnamed: 0", axis=1, inplace=True)
+    page = pagination(file)
+    AgGrid(file, width='100%', theme='streamlit', fit_columns_on_grid_load=True,
+           key='weeklyMetric', gridOptions=page)
 
 elif intervals == 'Monthly':
     file = pd.read_csv('ARIMAMetrics/ARIMA-MONTHLY.csv')
-    file.drop("Unnamed: 0", axis=1)
-    AgGrid(file, key='monthlyMetric')
+    file.drop("Unnamed: 0", axis=1, inplace=True)
+    page = pagination(file)
+    AgGrid(file, key='monthlyMetric', fit_columns_on_grid_load=True,
+           enable_enterprise_modules=True, theme='streamlit', gridOptions=page)
 
 elif intervals == 'Quarterly':
     file = pd.read_csv('ARIMAMetrics/ARIMA-QUARTERLY.csv')
-    file.drop("Unnamed: 0", axis=1)
-    AgGrid(file, key='quarterlyMetric')
+    file.drop("Unnamed: 0", axis=1, inplace=True)
+    page = pagination(file)
+    AgGrid(file, key='quarterlyMetric', fit_columns_on_grid_load=True,
+           enable_enterprise_modules=True, theme='streamlit', gridOptions=page)
 
-elif intervals == 'Daily':
-    file = pd.read_csv('ARIMAMetrics/ARIMA-DAILY.csv')
-    file.drop("Unnamed: 0", axis=1)
-    AgGrid(file, key='dailyMetric')
+# elif intervals == 'Daily':
+#     file = pd.read_csv('ARIMAMetrics/ARIMA-DAILY.csv')
+#     file.drop("Unnamed: 0", axis=1,inplace=True)
+#     page = pagination(file)
+#     AgGrid(file, key='dailyMetric', width='100%',fit_columns_on_grid_load=True,
+#            enable_enterprise_modules=True, theme='streamlit', gridOptions=page)
 
 # MODEL OUTPUT TABLE
 st.header("Model Output (Close Prices vs. Predicted Prices)")
@@ -99,44 +147,48 @@ interval = st.selectbox("Select Interval:", ('Weekly',
 
 if interval == 'Weekly':
     file = pd.read_csv('bestWeekly.csv')
-   
-    AgGrid(file, key='weeklycombined')
+    page = pagination(file)
+    AgGrid(file, key='weeklycombined', fit_columns_on_grid_load=True,
+           enable_enterprise_modules=True, theme='streamlit', gridOptions=page)
 
     # Visualization
     st.header("Visualization")
-    fig = px.line(file, x=file["Date"], y=["Close Prices", "ARIMA_50.0_(0, 1, 1)_Predictions", "LSTM_80.0_Predictions"], title="BOTH PREDICTED BRENT CRUDE OIL PRICES", width=1000)
+    fig = px.line(file, x=file["Date"], y=["Close Prices", "ARIMA_50.0_(1, 0, 0)_Predictions",
+                  "LSTM_60.0_Predictions"], title="BOTH PREDICTED BRENT CRUDE OIL PRICES", width=1000)
     st.plotly_chart(fig, use_container_width=True)
 
 
 elif interval == 'Monthly':
     file = pd.read_csv('bestMonthly.csv')
-    
-    AgGrid(file, key='monthlyCombined')
+    page = pagination(file)
+    AgGrid(file, key='monthlyCombined', fit_columns_on_grid_load=True,
+           enable_enterprise_modules=True, theme='streamlit', gridOptions=page)
     # Visualization
     st.header("Visualization")
-    fig = px.line(file, x=file["Date"], y=["Close Prices", "ARIMA_50.0_(0, 1, 0)_Predictions", #find file
-                  "LSTM_65.0_Predictions"], title="BOTH PREDICTED BRENT CRUDE OIL PRICES", width=1000)
+    fig = px.line(file, x=file["Date"], y=["Close Prices", "ARIMA_60.0_(0, 1, 1)_Predictions",  # find file
+                  "LSTM_80.0_Predictions"], title="BOTH PREDICTED BRENT CRUDE OIL PRICES", width=1000)
     st.plotly_chart(fig, use_container_width=True)
 
 
 elif interval == 'Quarterly':
     file = pd.read_csv('bestQuarterly.csv')
-    
-    AgGrid(file, key='quarterlyCombined')
+    page = pagination(file)
+    AgGrid(file, key='quarterlyCombined', fit_columns_on_grid_load=True,
+           enable_enterprise_modules=True, theme='streamlit', gridOptions=page)
     # Visualization
     st.header("Visualization")
-    fig = px.line(file, x=file["Date"], y=["Close Prices", "ARIMA_50.0_(0, 1, 0)_Predictions", #find file
+    fig = px.line(file, x=file["Date"], y=["Close Prices", "ARIMA_50.0_(0, 1, 0)_Predictions",  # find file
                   "LSTM_80.0_Predictions"], title="BOTH PREDICTED BRENT CRUDE OIL PRICES", width=1000)
     st.plotly_chart(fig, use_container_width=True)
 
 
-elif interval == 'Daily':
-    file = pd.read_csv('bestDaily.csv')
-    
-    AgGrid(file, key='dailyCombined')
-     # Visualization
-    st.header("Visualization")
-    fig = px.line(file, x=file["Date"], y=["Close Prices", "ARIMA_50.0_(0, 1, 1)_Predictions", #find file
-                "LSTM_65.0_Predictions"],title="BOTH PREDICTED BRENT CRUDE OIL PRICES", width=1000)
-    st.plotly_chart(fig, use_container_width=True)
+# elif interval == 'Daily':
+#     file = pd.read_csv('bestDaily.csv')
 
+#     AgGrid(file, key='dailyCombined', fit_columns_on_grid_load=True,
+#            enable_enterprise_modules=True, theme='streamlit', gridOptions=page)
+#     # Visualization
+#     st.header("Visualization")
+#     fig = px.line(file, x=file["Date"], y=["Close Prices", "ARIMA_50.0_(0, 1, 1)_Predictions",  # find file
+#                                            "LSTM_65.0_Predictions"], title="BOTH PREDICTED BRENT CRUDE OIL PRICES", width=1000)
+#     st.plotly_chart(fig, use_container_width=True)
