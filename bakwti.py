6d1731ce1e02e5e98ffd6ebeccbf40de70de0145
Janrswong <janrswong@addu.edu.ph>
2022-09-27 00:06:21 +0800

    updated tables
    Co-authored-by: Aly Castillon <aacast@users.noreply.github.com>
    Co-authored-by: Stephanie Lorraine Ignas <ignasstephanie@users.noreply.github.com>

--- bakwti.py
+++ bakwti.py
@@ -127,12 +127,35 @@ def displayWTI():
                    enable_enterprise_modules=True, theme='streamlit', gridOptions=page)
 
     # TABLES
-    # BRENT WTI
-    st.header("Brent vs. WTI Comparison")
-    st.subheader('ARIMA Accuracy Metrics & Best Models')
     df2 = pd.DataFrame([[0.8, (0, 1, 0), 2.427, 0.017, 0.8, (0, 1, 0), 5.211, 0.023], [np.nan, np.nan, np.nan, np.nan, 0.5, (0, 1, 0), 9.498, 0.042], [0.5, (1, 0, 0), 9.366, 0.039, 0.500000, (1, 0, 0), 9.530000, 0.042000], [np.nan, np.nan, np.nan, np.nan, 0.500000, (0, 1, 0), 41.668000, 0.097000], [0.600000, (0, 1, 1), 46.308000, 0.091000, 0.600000, (0, 1, 1), 45.242000, 0.099000]], index=pd.Index(
         ['Daily', 'Weekly*', 'Weekly', 'Monthly*', "Monthly"], name='Actual Label:'),
+        # columns=pd.MultiIndex.from_product([['Brent', 'WTI'], ['Train Split', 'Order', 'MSE', 'MAPE']], names=['Model:', 'Predicted:']))
+        # columns=pd.MultiIndex.from_tuples([("Brent", "Train Split"), ("Brent", "Order"), ("Brent", "MSE"), ("Brent", "MAPE"),
+        #                                    ("WTI ", "Train Split"), ("WTI", "Order"), ("WTI", "MSE"), ("WTI", "MAPE")]))
         columns=(["Brent Train Split", "Brent Order", "Brent MSE", "Brent MAPE", "WTI Train Split", "WTI Order", "WTI MSE", "WTI MAPE"]))
+
+    # df2 = pd.DataFrame([[0.8, (0, 1, 0), 2.427, 0.017, 0.8, (0, 1, 0), 5.211, 0.023], [0.5, (1, 0, 0), 9.366, 0.039, 0.5, (0, 1, 0), 9.498, 0.042], [np.nan, np.nan, np.nan, np.nan, 0.5, (0, 1, 0), 9.498, 0.042], [0.5, (1, 0, 0), 9.366, 0.039, 0.5, (0, 1, 0), 9.498, 0.042]], index=pd.Index(
+    #     ['Daily', 'Weekly', '', 'Monthly'], name='Actual Label:'),
+    #     columns=pd.MultiIndex.from_product([['', '1'], ['Train Split', 'Order', 'MSE', 'MAPE']], names=['Model:', 'Predicted:']))
+    st.table(df2)
+    # multi_index = pd.MultiIndex.from_tuples(
+    #     [('Daily'), ('Weekly'), ('Hello World'), ('Monthly')], names=['Courses', 'Courses1', 'Courses2', 'Courses3'])
+    # col = pd.MultiIndex.from_tuples([("Brent", "Train Split"), ("Brent", "Order"), ("Brent", "MSE"), (
+    #     "Brent", "MAPE"), ("WTI ", "Train Split"), ("WTI", "Order"), ("WTI", "MSE"), ("WTI", "MAPE")])
+    # data = [[0.8, (0, 1, 0), 2.427, 0.017, 0.8, (0, 1, 0), 5.211, 0.023], [0.5, (1, 0, 0), 9.366, 0.039, 0.5, (0, 1, 0), 9.498, 0.042], [
+    #     0, 0, 0, 0, 0.5, (0, 1, 0), 9.498, 0.042], [0.5, (1, 0, 0), 9.366, 0.039, 0.5, (0, 1, 0), 9.498, 0.042]]
+    # df2 = pd.DataFrame(data, columns=col, index=multi_index)
+
+    # multi_index = pd.MultiIndex.from_tuples([("r0", "rA"),
+    #                                          ("r1", "rB")],
+    #                                         names=['Courses', 'Fee'])
+    # cols = pd.MultiIndex.from_tuples([("Gasoline", "Toyoto"),
+    #                                   ("Gasoline", "Ford"),
+    #                                   ("Electric", "Tesla"),
+    #                                   ("Electric", "Nio")])
+    # data = [[100, 300, 900, 400], [200, 500, 300, 600]]
+
+    # df2 = pd.DataFrame(data, columns=cols, index=multi_index)
 
     cell_hover = {  # for row hover use <tr> instead of <td>
         'selector': 'tr:hover',
@@ -144,23 +167,83 @@ def displayWTI():
         'props': 'font-style: italic; color: darkgrey; font-weight:normal;'
     }
     headers = {
+        # 'selector': 'th:not(.index_name)',
         'selector': 'th:not(.index_name)',
         'props': 'background-color: #f0f2f6; color: black;'
     }
     df2 = df2.style
     df2 = df2.set_table_styles(
         [cell_hover, index_names, headers]).highlight_null(props="color: transparent;")
+    df2 = df2.set_table_styles([
+        {'selector': 'th.col_heading', 'props': 'text-align: center;'},
+        {'selector': 'th.col_heading.level0', 'props': 'font-size: 1em;'},
+        {'selector': 'td', 'props': 'text-align: center; font-weight: bold;'},
+    ], overwrite=False)
+    # df2 = df2.replace(np.nan, '', regex=True)
     st.table(df2)
 
-    # LSTM
-    st.subheader('LSTM Accuracy Metrics & Best Models')
-    df3 = pd.DataFrame([[0.6, 4.152, 0.021, 0.8, 5.904, 0.02], [0.8, 21.62, 0.037, 0.8, 25.012, 0.039], [0.8, 56.275, 0.075, 0.8, 80.147, 0.096]], index=pd.Index(
-        ['Daily', 'Weekly', "Monthly"], name='Actual Label:'),
-        columns=(["Brent Train Split", "Brent MSE", "Brent MAPE", "WTI Train Split", "WTI MSE", "WTI MAPE"]))
-    df3 = df3.style
-    df3 = df3.set_table_styles(
-        [cell_hover, index_names, headers])
-    st.table(df3)
+    # st.table(sss)
+    sss = pd.read_csv('WTI/CopBook1.csv')
+    # sss = sss.replace(np.nan, '', regex=True)
+    sss.rename(columns={'Unnamed: 0': ' '}, inplace=True)
+    sss.fillna("")
+    # sss = sss.style
+
+    # AgGrid(sss, key='WTI/CopBook1.csv', fit_columns_on_grid_load=True,
+    #    enable_enterprise_modules=True, theme='streamlit')
+    cell_hover = {  # for row hover use <tr> instead of <td>
+        'selector': 'td:hover',
+        'props': [('background-color', '#ffffb3')]
+    }
+
+    # sss = sss.style.set_properties(**{'background-color': 'black',
+    #                    'color': 'green'})
+    # sss = sss.style.set_properties(**{'background-color': 'yellow' if v ==
+    #                                sss.loc[0] else "" for v in sss}, axis=1).highlight_null(props="color: transparent;")
+
+    # sss = sss.style.apply(lambda x: ["background: red" if v ==
+    #                                  (x.iloc[1,3]) else "" for v in x], axis=1).highlight_null(props="color: transparent;")
+
+    # sss = sss.style.apply(lambda x: ["background: red"(
+    #     (x.iloc[1:3]))]).highlight_null(props="color: transparent;")
+
+    # sss.style.apply(lambda x: ["background: red" if v ==
+    #                 x.loc[0] else "" for v in x], axis=1)
+    sss = sss.style
+    sss = sss.set_table_styles(
+        [cell_hover, index_names, headers]).highlight_null(props="color: transparent;")
+    sss = sss.set_table_styles([
+        {'selector': 'th.col_heading', 'props': 'text-align: center;'},
+        {'selector': 'th.col_heading.level0', 'props': 'font-size: 1em;'},
+        {'selector': 'td', 'props': 'text-align: center; font-weight: bold;'},
+    ], overwrite=False)
+
+    # sss = sss.style.highlight_null(props="color: transparent;")
+    # sss = sss.set_table_styles([cell_hover])
+
+    # def highlight_max(x):
+    #     return ['font-weight: bold' if v == x.loc[0] else ''
+    #             for v in x]
+    # sss = sss.style.apply(highlight_max)
+
+    st.table(sss)
+
+    # BRENT WTI
+    st.header("Brent vs. WTI Accuracy Metrics & Best Models")
+
+    # arima = Image.open('assets/images/ARIMA23.png')
+    # st.image(arima, caption='Table of Comparisons: ARIMA',
+    #          use_column_width='auto')
+
+    col1, col2, col3 = st.columns([1, 6, 1])
+
+    with col2:
+        arima = Image.open('assets/images/ARIMA3111.png')
+        st.image(arima, caption='Table of Comparisons: ARIMA',
+                 use_column_width='auto')
+        lstm = Image.open('assets/images/LSTM2.png')
+        st.image(lstm, caption='Table of Comparisons: LSTM',
+                 use_column_width='auto')
 
     # MODEL OUTPUT TABLE
     st.header("Model Output (Close Prices vs. Predicted Prices)")
